0fe5d5ccdd6e99827b5551af9cff20ecc514a8e0
Éric Massé <Ericmas001@gmail.com>
2017-07-17 22:02:19 -0400

    Making LoggingDb a little more unity friendly

---
+++ Src/Com.Ericmas001.LoggingDb/LoggingDbUnityConfig.cs
@@ -0,0 +1,20 @@
+﻿using Com.Ericmas001.LoggingDb.Services;
+using Com.Ericmas001.LoggingDb.Services.Interfaces;
+using Microsoft.Practices.Unity;
+
+namespace Com.Ericmas001.LoggingDb
+{
+    public static class LoggingDbUnityConfig
+    {
+        public static IUnityContainer Container { get; private set; }
+
+        public static void RegisterTypes(IUnityContainer container = null)
+        {
+            Container = container ?? new UnityContainer();
+
+            Container.RegisterType<ILoggingDbContext, LoggingDbContext>();
+
+            Container.RegisterType<ILogWriterService, LogWriterService>();
+        }
+    }
+}
